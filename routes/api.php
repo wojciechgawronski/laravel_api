e210740fc715188d65e22f49ca8da5d0c90dfc43
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-07-09 08:42:19 +0200

    oAuth install and  Postman url endpoints and authorization prepare

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// POSTMAN: http://127.0.0.1:8000/api/test
+Route::get('/test', function(Request $request){
+    return 'api test';
+});
 
+// POSTMAN: http://127.0.0.1:8000/api/v1/user
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+// POSTMAN: http://127.0.0.1:8000/api/user
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
